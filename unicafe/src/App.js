d5bf18c320c0d76c0211ef81f6963e0387668261
bgocap <diego.capcha98@hotmail.com>
2023-07-13 18:23:38 +0300

    Exercise 1.8

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,8 +1,19 @@
 import { useState } from 'react'
 
-const Button = ({ text,handleClick }) =>
-(<button onClick={handleClick} >{text}</button>)
-
+// a proper place to define a component
+const Statistics = ({Good,Neutral,Bad,All,Average,Positive}) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <div>good: {Good}</div>
+      <div>neutral: {Neutral}</div>
+      <div>bad: {Bad}</div>
+      <div>all:{All}</div>
+      <div>average: {Average}</div>
+      <div>positive: {Positive} %</div>
+    </div>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -12,25 +23,21 @@ const App = () => {
   const [all,setAll]=useState(0)
   const [score,setScore]=useState(0)
 
+  const handleGood = () =>{setGood(good+1);setAll(all+1);setScore(score+1)}
+  const handleNeutral = () => {setNeutral(neutral+1);setAll(all+1);setScore(score+0)}
+  const handleBad = () =>{setBad(bad+1);setAll(all+1);setScore(score-1)}
+
   return (
     <div>
       <h1>give feedback</h1>
-      <Button 
-      handleClick={()=>{setGood(good+1);setAll(all+1);setScore(score+1)}}
-      text='Good' />
-      <Button
-      handleClick={()=>{setNeutral(neutral+1);setAll(all+1);setScore(score+0)}}
-      text='Neutral' />
-      <Button 
-      handleClick={()=>{setBad(bad+1);setAll(all+1);setScore(score-1)}}
-      text='Bad' />
-      <h1>statistics</h1>
-      <div>good: {good}</div>
-      <div>neutral: {neutral}</div>
-      <div>bad: {bad}</div>
-      <div>all: {all}</div>
-      <div>average: {score/all}</div>
-      <div>positive: {(100*good)/all}%</div>
+      <button onClick={handleGood} >Good</button>
+      <button onClick={handleNeutral} >Neutral</button>
+      <button onClick={handleBad} >Bad</button>
+      < Statistics 
+      Good={good} Neutral={neutral} Bad={bad} All={all} 
+      Average={score/all} 
+      Positive={(100*good)/all} 
+      />
     </div>
   )
 }
