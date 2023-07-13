c92973fa455db6d2befbbaca3af52592315f86b6
bgocap <diego.capcha98@hotmail.com>
2023-07-13 17:15:49 +0300

    Exercise 1.7

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -9,25 +9,28 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  //const [all,setAll]=useState(0)
+  const [all,setAll]=useState(0)
+  const [score,setScore]=useState(0)
 
   return (
     <div>
       <h1>give feedback</h1>
       <Button 
-      handleClick={()=>{setGood(good+1)}}
+      handleClick={()=>{setGood(good+1);setAll(all+1);setScore(score+1)}}
       text='Good' />
       <Button
-      handleClick={()=>{setNeutral(neutral+1)}}
+      handleClick={()=>{setNeutral(neutral+1);setAll(all+1);setScore(score+0)}}
       text='Neutral' />
       <Button 
-      handleClick={()=>{setBad(bad+1)}}
+      handleClick={()=>{setBad(bad+1);setAll(all+1);setScore(score-1)}}
       text='Bad' />
       <h1>statistics</h1>
       <div>good: {good}</div>
       <div>neutral: {neutral}</div>
       <div>bad: {bad}</div>
+      <div>all: {all}</div>
+      <div>average: {score/all}</div>
+      <div>positive: {(100*good)/all}%</div>
     </div>
   )
 }
