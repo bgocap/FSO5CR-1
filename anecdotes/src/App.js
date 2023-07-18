49af99910a8563602749b83a6cdf8aa982fe0b73
bgocap <diego.capcha98@hotmail.com>
2023-07-18 11:57:10 +0300

    Exercise 1.14

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -13,31 +13,31 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-  //const points = new Array(8).fill(0)
 
   const [selected, setSelected] = useState(0)
+
   const [allPoints, setVotes] = useState(new Array(anecdotes.length).fill(0))
-
-  //console.log(allPoints)
-
-  
 
   const selectRandomNote = () =>{
     setSelected(Math.floor(Math.random() * anecdotes.length))
   }
+
   const addVote = () =>{
-    const allPointsCopy = {...allPoints}
+    const allPointsCopy = [...allPoints]
     allPointsCopy[selected]=allPointsCopy[selected]+1
     setVotes(allPointsCopy)
-    console.log(allPoints)
   }
+
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p style={{fontSize:24,fontStyle:'italic'}}>{anecdotes[selected]}</p><p style={{fontSize:24}}>Has {allPoints[selected]} vote(s)</p>
       <div>
         <Button handleClick={addVote} text='vote'/>
         <Button handleClick={selectRandomNote} text='next anecdote'/>
       </div>
+      <h2>Anectdote with the most votes</h2>
+      <p style={{fontSize:18,fontStyle:'italic'}}>{anecdotes[allPoints.indexOf(Math.max(...allPoints))]}</p>
     </div>
   )
 }
